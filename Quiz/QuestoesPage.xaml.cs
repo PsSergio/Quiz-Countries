810373913a3298970e0f0c6d3b992053e753d7ec
Paulo Sergio <paulosergio@macsergio.local>
2024-02-14 12:48:49 -0300

    feat: adicionando lógica das questoes

--- Quiz/QuestoesPage.xaml.cs
+++ Quiz/QuestoesPage.xaml.cs
@@ -8,11 +8,11 @@ public partial class QuestoesPage : ContentPage
 {
 	public QuestoesPage()
 	{
-		
+
 		InitializeComponent();
 	}
 
-    protected override void OnAppearing()
+	protected override void OnAppearing()
 	{
 		base.OnAppearing();
 
@@ -40,32 +40,32 @@ public partial class QuestoesPage : ContentPage
 
 		bool IsTimeOut = false;
 
-		while(!IsTimeOut)
+		while (!IsTimeOut)
 		{
 
 			minutes -= 1;
 
 			seconds = 59;
 
-			while(seconds != 0)
+			while (seconds != 0)
 			{
 
 				seconds -= 1;
 
-				if(seconds >= 10)
+				if (seconds >= 10)
 				{
-                    labelTime.Text = minutes + ":" + seconds;
-                }else
+					labelTime.Text = minutes + ":" + seconds;
+				} else
 				{
-                    labelTime.Text = minutes + ":" + "0" + seconds;
+					labelTime.Text = minutes + ":" + "0" + seconds;
 
-                }
+				}
 
-                await Task.Delay(1000);
+				await Task.Delay(1000);
 
 			}
 
-			if(minutes == 0)
+			if (minutes == 0)
 			{
 				IsTimeOut = true;
 			}
@@ -75,13 +75,109 @@ public partial class QuestoesPage : ContentPage
 
 	}
 
-    void btnNextQuestion_Clicked(System.Object sender, System.EventArgs e)
+	int countQuestions = 0;
+	int totalPoints = 0;
+	bool isCorrect = false, marcou = false;
+
+    async void btnNextQuestion_Clicked(System.Object sender, System.EventArgs e)
     {
+
+        if (countQuestions == 1)
+		{
+			imgQuestion.Source = "question2_flag";
+
+			ans1.Content = "England";
+			ans3.Content = "Saudi Arabia";
+			ans2.Content = "Germany";
+			ans4.Content = "Japan";
+
+			ans1.Value = "wrong";
+			ans3.Value = "correct";
+			ans2.Value = "wrong";
+			ans4.Value = "wrong";
+
+
+		}else if (countQuestions == 2)
+		{
+            imgQuestion.Source = "question3_flag";
+
+            ans1.Content = "Tanzania";
+            ans2.Content = "Italy";
+            ans3.Content = "Colombia";
+            ans4.Content = "England";
+
+            ans1.Value = "wrong";
+            ans2.Value = "wrong";
+            ans3.Value = "wrong";
+            ans4.Value = "correct";
+        }
+        else if (countQuestions == 3)
+        {
+            imgQuestion.Source = "question4_flag";
+
+            ans1.Content = "Kenya";
+            ans2.Content = "South Korea";
+            ans3.Content = "Poland";
+            ans4.Content = "Argentina";
+
+            ans1.Value = "correct";
+            ans2.Value = "wrong";
+            ans3.Value = "wrong";
+            ans4.Value = "wrong";
+        }
+        else if (countQuestions == 4)
+        {
+            imgQuestion.Source = "question5_flag";
+
+            ans1.Content = "Canada";
+            ans2.Content = "Qatar";
+            ans3.Content = "Ghana";
+            ans4.Content = "Nepal";
+
+            ans1.Value = "wrong";
+            ans2.Value = "correct";
+            ans3.Value = "wrong";
+            ans4.Value = "wrong";
+
+			
+        }else if(countQuestions == 5)
+		{
+            await SecureStorage.SetAsync("totalPoints", totalPoints.ToString());
+
+            await Navigation.PushAsync(new ReportPage());
+        }
+
+        countQuestions++;
+
+        if (isCorrect)
+		{
+			totalPoints++;
+		}
+
     }
 
-    void RadioButton_CheckedChanged(System.Object sender, Microsoft.Maui.Controls.CheckedChangedEventArgs e)
-    {
+    private void verificarResposta(System.Object sender, System.EventArgs e)
+	{
+		if (marcou)
+		{
+			marcou = false;
+		}
+		else
+		{
+			RadioButton opcao = sender as RadioButton;
+			string valorOpcao = opcao.Value.ToString();
+
+			if (valorOpcao.Contains("correct"))
+			{
+				isCorrect = true;
+			}
+			else
+			{
+				isCorrect = false;
+			}
+
+			marcou = true;
+		}
+        
     }
-
-
 }
---
+++ Quiz/ReportPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace Quiz;
+
+public partial class ReportPage : ContentPage
+{
+	public ReportPage()
+	{
+		InitializeComponent();
+
+
+	}
+
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        labelTotal.Text = await SecureStorage.Default.GetAsync("totalPoints");
+
+
+    }
+}
